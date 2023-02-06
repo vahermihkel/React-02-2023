98c9318ce292fbd3c78f29d6ae72a7baf831ec51
mihkelvaher <mihkel.vaher@industry62.com>
2023-02-06 16:01:34 +0200

    Reacti uue projekti loomine

---
+++ veebipood/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" src="https://nobecars.com/wp-content/uploads/2022/02/sergeizjuganov_IMG_5728-red-scaled.jpg" alt="" />
+      <button className="nupp">Nupp</button>
+    </div>
+  );
+}
+
+export default App;
